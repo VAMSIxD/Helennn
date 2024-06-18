6a870875adf0134e8b8b7e0844aa52214a9d0ca1
VAMSI REDDY <129572799+VAMSIxD@users.noreply.github.com>
2024-06-18 14:43:22 +0530

    Update resume.py

--- Pokemonxd/plugins/admins/resume.py
+++ Pokemonxd/plugins/admins/resume.py
@@ -16,17 +16,22 @@ RESUME_COMMAND = get_command("RESUME_COMMAND")
 @bot.on_message(
     command(RESUME_COMMAND)
     & filters.group
-    & ~filters.edited
     & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
+    if message.edit_date:
+        return  # Skip processing if the message has been edited
+    
+    if len(message.command) != 1:
         return await message.reply_text(_["general_2"])
+    
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
+    
     await music_on(chat_id)
     await poke.resume_stream(chat_id)
+    
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention)
     )
