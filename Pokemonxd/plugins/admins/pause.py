4fa1c44eb98195ce05a4aa78aa7601d3662ccbc7
VAMSI REDDY <129572799+VAMSIxD@users.noreply.github.com>
2024-06-18 14:41:28 +0530

    Update pause.py

--- Pokemonxd/plugins/admins/pause.py
+++ Pokemonxd/plugins/admins/pause.py
@@ -16,17 +16,23 @@ PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 @bot.on_message(
     command(PAUSE_COMMAND)
     & filters.group
-    & ~filters.edited
     & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
+    if message.edit_date:
+        return  # Skip processing if the message has been edited
+    
+    if len(message.command) != 1:
         return await message.reply_text(_["general_2"])
+    
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
+    
     await music_off(chat_id)
     await poke.pause_stream(chat_id)
+    
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention)
     )
+    
