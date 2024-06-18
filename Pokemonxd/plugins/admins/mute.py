f1ddefa6f0dcd639c0eb0c57064f368901980acf
VAMSI REDDY <129572799+VAMSIxD@users.noreply.github.com>
2024-06-18 14:39:17 +0530

    Update mute.py

--- Pokemonxd/plugins/admins/mute.py
+++ Pokemonxd/plugins/admins/mute.py
@@ -15,17 +15,22 @@ MUTE_COMMAND = get_command("MUTE_COMMAND")
 @bot.on_message(
     filters.command(MUTE_COMMAND)
     & filters.group
-    & ~filters.edited
     & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def mute_admin(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1 or message.reply_to_message:
+    if message.edit_date:
+        return  # Skip processing if the message has been edited
+    
+    if len(message.command) != 1 or message.reply_to_message:
         return await message.reply_text(_["general_2"])
+    
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"])
+    
     await mute_on(chat_id)
     await poke.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention)
     )
+    
