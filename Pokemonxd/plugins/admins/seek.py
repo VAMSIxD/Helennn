c33fc9c577ab9e4e1c7d900cdcb3c80007c5b5ba
VAMSI REDDY <129572799+VAMSIxD@users.noreply.github.com>
2024-06-18 14:45:32 +0530

    Update seek.py

--- Pokemonxd/plugins/admins/seek.py
+++ Pokemonxd/plugins/admins/seek.py
@@ -16,28 +16,36 @@ SEEK_COMMAND = get_command("SEEK_COMMAND")
 @bot.on_message(
     filters.command(SEEK_COMMAND)
     & filters.group
-    & ~filters.edited
     & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def seek_comm(cli, message: Message, _, chat_id):
+    if message.edit_date:
+        return  # Skip processing if the message has been edited
+    
     if len(message.command) == 1:
         return await message.reply_text(_["admin_28"])
+    
     query = message.text.split(None, 1)[1].strip()
     if not query.isnumeric():
         return await message.reply_text(_["admin_29"])
+    
     playing = db.get(chat_id)
     if not playing:
         return await message.reply_text(_["queue_2"])
+    
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
         return await message.reply_text(_["admin_30"])
+    
     file_path = playing[0]["file"]
     if "index_" in file_path or "live_" in file_path:
         return await message.reply_text(_["admin_30"])
+    
     duration_played = int(playing[0]["played"])
     duration_to_skip = int(query)
     duration = playing[0]["dur"]
+    
     if message.command[0][-2] == "c":
         if (duration_played - duration_to_skip) <= 10:
             return await message.reply_text(
@@ -47,20 +55,21 @@ async def seek_comm(cli, message: Message, _, chat_id):
             )
         to_seek = duration_played - duration_to_skip + 1
     else:
-        if (
-            duration_seconds - (duration_played + duration_to_skip)
-        ) <= 10:
+        if (duration_seconds - (duration_played + duration_to_skip)) <= 10:
             return await message.reply_text(
                 _["admin_31"].format(
                     seconds_to_min(duration_played), duration
                 )
             )
         to_seek = duration_played + duration_to_skip + 1
+    
     mystic = await message.reply_text(_["admin_32"])
+    
     if "vid_" in file_path:
         n, file_path = await YouTube.video(playing[0]["vidid"], True)
         if n == 0:
             return await message.reply_text(_["admin_30"])
+    
     try:
         await poke.seek_stream(
             chat_id,
@@ -71,10 +80,13 @@ async def seek_comm(cli, message: Message, _, chat_id):
         )
     except:
         return await mystic.edit_text(_["admin_34"])
+    
     if message.command[0][-2] == "c":
         db[chat_id][0]["played"] -= duration_to_skip
     else:
         db[chat_id][0]["played"] += duration_to_skip
+    
     await mystic.edit_text(
         _["admin_33"].format(seconds_to_min(to_seek))
     )
+    
